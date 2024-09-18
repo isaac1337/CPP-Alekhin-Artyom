2ecf9d1c29373b8812f17ee736ab8ebeca9e2a60
Isaac1337 <alekhin.aa@phystech.edu>
2024-09-18 22:59:25 +0300

    hw array

---
+++ seminar 2/1.7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a[1000];
+    int n;
+    scanf("%i", &n);
+    for (int i = 0; i < n; ++i)
+        scanf("%i", &a[i]);
+    for (int i = n; i >= 0; i--)
+    {
+        a[2*i] = a[i];
+        a[2*i+1] = a[i];
+    }
+    for (int i = 0; i < 2*n; ++i)
+        printf("%i ", a[i]);
+        
+    printf("\n");
+}
