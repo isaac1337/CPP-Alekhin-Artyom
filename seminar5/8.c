7e5287881ae6d0b1ea6162364b885f9b9d36e8c4
Isaac1337 <alekhin.aa@phystech.edu>
2024-10-14 18:05:57 +0300

    segments hw

---
+++ seminar5/8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// Тут нужно написать функцию adder
+
+int adder(int a)
+{
+    static int sum = 0;
+    sum += a;
+    return sum;
+}
+
+int main()
+{
+    printf("%i\n", adder(10)); // Должен напечатать 10
+    printf("%i\n", adder(15)); // Должен напечатать 25
+    printf("%i\n", adder(70)); // Должен напечатать 95
+}
